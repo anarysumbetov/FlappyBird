604fc44eeeac0d75adb1f12f68314c0975be7ce0
anarysumbetov <74860324+anarysumbetov@users.noreply.github.com>
2021-03-18 15:04:42 +0600

    Finished

--- App.js
+++ App.js
@@ -1,14 +1,142 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text, Dimensions, TouchableWithoutFeedback } from 'react-native'
+import Bird from './components/Bird'
+import Obstacles from './components/Obstacles'
 
 export default function App() {
+  const screenWidth = Dimensions.get("screen").width
+  const screenHeight = Dimensions.get("screen").height
+  const birdLeft = screenWidth / 2
+  const [birdBottom, setBirdBottom ] = useState(screenHeight/2) 
+  const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth)
+  const [obstaclesLeftTwo, setObstaclesLeftTwo] = useState(screenWidth + screenWidth/2 + 30)
+  const [obstaclesNegHeight, setObstaclesNegHeight] = useState(0)
+  const [obstaclesNegHeightTwo, setObstaclesNegHeightTwo] = useState(0)
+  const [score, setScore] = useState(0)
+  const obstacleWidth = 60
+  const obstacleHeight = 300
+  const gap = 200
+  const gravity = 3
+  let gameTimerId
+  let obstaclesLeftTimerId
+  let obstaclesLeftTimerIdTwo
+  const [isGameOver, setIsGameOver] = useState(false)
+
+  //start bird falling
+  useEffect(() => {
+    if (birdBottom > 0 ) {
+      gameTimerId = setInterval(() => {
+        setBirdBottom(birdBottom => birdBottom - gravity)
+      }, 30)
+
+      return () => {
+        clearInterval(gameTimerId)
+      }
+    }
+  }, [birdBottom])
+  console.log(birdBottom)
+
+  const jump = () => {
+    if (!isGameOver && (birdBottom < screenHeight)) {
+      setBirdBottom(birdBottom => birdBottom + 50)
+      console.log('jumped')
+    }
+  }
+
+  //start first obstacles
+  useEffect(() => {
+    if (obstaclesLeft > -obstacleWidth) {
+      obstaclesLeftTimerId = setInterval(() => {
+        setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
+      }, 30)
+      return () => {
+        clearInterval(obstaclesLeftTimerId)
+      }
+    } else {
+      setObstaclesLeft(screenWidth)
+      setObstaclesNegHeight( - Math.random() * 100)
+      setScore(score => score + 1)
+    }
+  }, [obstaclesLeft])
+
+  //start second obstacles
+  useEffect(() => {
+    if (obstaclesLeftTwo > -obstacleWidth) {
+      obstaclesLeftTimerIdTwo = setInterval(() => {
+        setObstaclesLeftTwo(obstaclesLeftTwo => obstaclesLeftTwo - 5)
+      }, 30)
+      return () => {
+        clearInterval(obstaclesLeftTimerIdTwo)
+      }
+    } else {
+      setObstaclesLeftTwo(screenWidth)
+      setObstaclesNegHeightTwo( - Math.random() * 100)
+      setScore(score => score + 1)
+    }
+  }, [obstaclesLeftTwo])
+
+  //check for collisions
+  useEffect(() => {
+    if (
+    ((birdBottom < (obstaclesNegHeight + obstacleHeight + 30) ||
+    birdBottom > (obstaclesNegHeight + obstacleHeight + gap -30 )) &&
+    (obstaclesLeft > screenWidth/2 -30 && obstaclesLeft < screenWidth/2 + 30)
+    )
+    ||
+    ((birdBottom < (obstaclesNegHeightTwo + obstacleHeight + 30) ||
+    birdBottom > (obstaclesNegHeightTwo + obstacleHeight + gap -30 )) &&
+    (obstaclesLeftTwo > screenWidth/2 -30 && obstaclesLeftTwo < screenWidth/2 + 30)
+    ) 
+    )
+    {
+
+      console.log('game over')
+      gameOver()
+      
+    }
+    
+  })
+
+  const gameOver = () => {
+    clearInterval(gameTimerId)
+    clearInterval(obstaclesLeftTimerId)
+    clearInterval(obstaclesLeftTimerIdTwo)
+    setIsGameOver(true)
+  }
+
+
+
+
+
   return (
+    <TouchableWithoutFeedback onPress={jump}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isGameOver && <Text>{score}</Text>}
+      <Bird 
+        birdBottom={birdBottom}
+        birdLeft={birdLeft}
+      />
+      <Obstacles 
+        color={'green'}
+        obstacleWidth={obstacleWidth}
+        obstacleHeight={obstacleHeight}
+        randomBottom={obstaclesNegHeight}
+        gap={gap}
+        obstaclesLeft={obstaclesLeft}
+      />
+      <Obstacles 
+        color={'yellow'}
+        obstacleWidth={obstacleWidth}
+        obstacleHeight={obstacleHeight}
+        randomBottom={obstaclesNegHeightTwo}
+        gap={gap}
+        obstaclesLeft={obstaclesLeftTwo}
+      />
     </View>
-  );
+    </TouchableWithoutFeedback>
+    
+
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +146,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
